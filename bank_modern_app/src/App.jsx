439a9aa87d0fbfb3fa3782216b216d287f1323ca
Undefined <lirikrexhepiii@gmail.com>
2023-03-22 15:56:04 +0100

    Business Section

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -21,6 +21,7 @@ const App = () => (
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats/>
+        <br />
         <Business/>
         <Billing/> 
         <CardDeal/> 
